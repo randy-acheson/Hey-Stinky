1188f3cce6da6dc8480c678b5f8ac589c24a03d5
Mary Abbott <mabbott9@gatech.edu>
2021-02-20 03:39:38 -0500

    added interactive object interface

---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface InteractiveObject
+{
+    string getHoverMessage();
+
+    void OnPlayerInteract(GameObject player, byte actionNum);
+}
