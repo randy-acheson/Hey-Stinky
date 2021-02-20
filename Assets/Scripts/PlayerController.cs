213faa6c3a2ce56e3e14d49c2beabeaa448dc33d
Eric Johnson <johnson.eric321@gmail.com>
2021-02-19 22:32:32 -0500

    House and PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 8f;
+    public float gravity = -9.81f;
+    public float bounce = 0.04f;
+    public float mouseSensitivity = 0.1f;
+    
+    private Transform body;
+    private Transform camera;
+    private Transform hand;
+    private CharacterController controller;
+    private Light flashlight;
+    private float velY = 0f;
+    private float rotX = 0;
+    private float movement = 0f;
+    private bool isGrounded = true;
+
+    private float[] sendPacket = new float[6];
+    
+
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        camera = transform.GetChild(0);
+        body = transform.GetChild(1);
+        hand = camera.GetChild(0);
+        flashlight = hand.GetChild(0).GetComponent<Light>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.normal.y > 0.5){
+            isGrounded = true;
+        }else if(hit.normal.y < -0.9 && hit.moveDirection.y > 0 && velY > 0){
+            velY = 0;
+        }
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            UnityEditor.EditorApplication.isPlaying = false; 
+        }
+
+        if(!Physics.CheckSphere(transform.position, 0.1f)){
+            isGrounded = false;
+        }
+
+        if(Input.GetKey(KeyCode.LeftControl)){
+            speed = 8;
+        }else{
+            speed = 4;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift)){
+            speed = 8;
+        }else{
+            speed = 4;
+        }
+
+        if(isGrounded){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                velY = 4;
+                isGrounded = false;
+            }else{
+                velY = 0;
+            }
+        }else{
+            velY += gravity * Time.deltaTime;
+        }
+
+        float posX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float posZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        if(isGrounded && (posX !=0 || posZ != 0))
+        {
+            movement = (movement + 1.5f * Mathf.Max(Mathf.Abs(posX), Mathf.Abs(posZ))) % (Mathf.PI*2f);
+        }
+        else if(movement < Mathf.PI*0.5f)
+        {
+            movement = Mathf.Max(movement - 5f*Time.deltaTime, 0f);
+        }
+        else if(movement >= Mathf.PI*0.5f && movement < Mathf.PI)
+        {
+            movement = Mathf.Min(movement + 5f*Time.deltaTime, Mathf.PI);
+        }
+        else if(movement >= Mathf.PI && movement < Mathf.PI*1.5f)
+        {
+            movement = Mathf.Max(movement - 5f*Time.deltaTime, Mathf.PI);
+        }
+        else if(movement >= Mathf.PI*1.5f)
+        {
+            movement = Mathf.Min(movement + 5f*Time.deltaTime, Mathf.PI*2f);
+        }
+
+        float vertBob = Mathf.Abs(Mathf.Sin(movement + Mathf.PI*0.5f));
+        float horiBob = Mathf.Sin(movement);
+
+        float posY = velY * Time.deltaTime;
+
+        body.localPosition = new Vector3(horiBob*bounce*0.5f, 0.9f+vertBob*bounce, 0f);
+        //body.transform.localRotation = Quaternion.Euler(0f, 0f, horiBob*-0.0244f);
+        camera.localPosition = new Vector3(horiBob*bounce*0.5f, 1.68f+vertBob*bounce, 0f);
+
+        controller.Move(transform.right*posX + transform.forward*posZ + Vector3.up*posY);
+
+        sendPacket[0] = transform.position.x;
+        sendPacket[1] = transform.position.y;
+        sendPacket[2] = transform.position.z;
+
+        /////////////////////////////////
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //Debug.Log("MouseX: " + Input.GetAxis("Mouse X") + ", sens: " + mouseSensitivity + ", time: " + Time.deltaTime + ", total: " + mouseX);
+        rotX -= mouseY;
+        rotX = Mathf.Clamp(rotX, -85f, 85f);
+
+        camera.localRotation = Quaternion.Euler(rotX, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        sendPacket[3] = camera.localRotation.x;
+        sendPacket[4] = transform.rotation.y;
+
+        /////////////////////////////////
+    
+        hand.localRotation = Quaternion.Euler(
+            90f + 1.5f*(Mathf.PerlinNoise(0, 1f*Time.time)-0.5f), 
+            1.5f*(Mathf.PerlinNoise(1f*Time.time, 0)-0.5f), 
+            0f);
+
+        ////////////////////////////////
+
+        if(Input.GetKeyDown(KeyCode.E)){
+            flashlight.enabled = !flashlight.enabled;
+            if(flashlight.enabled){
+                sendPacket[5] = 1f;
+            }else{
+                sendPacket[5] = 0f;
+            }
+        }
+
+        float noise = Mathf.PerlinNoise(0, 10f*Time.time);
+
+        flashlight.intensity = Mathf.Min(40f + noise*160f, 80f);
+    }
+}
