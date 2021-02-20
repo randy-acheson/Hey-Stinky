69d11012aa836c7fe5f4647fdae27ae4134b1b27
Matthew Esch <mze@gmail.com>
2021-02-20 13:04:48 -0500

    The titans of tcp broadcasting

---
+++ Assets/Scripts/broadcastServer.py
@@ -0,0 +1,33 @@
+
+import socket
+import threading
+
+HOST_ADDR = "192.168.86.46"
+TCP_PORT = 7777
+
+subscribers = set()
+
+def serverStartup():
+    s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_tcp.bind((HOST_ADDR, TCP_PORT))
+    s_tcp.listen()
+
+    while True:
+        connection, client_addr = s_tcp.accept()
+        subscribers.add(connection)
+        t = threading.Thread(target=spawnTCPConnection, args=(connection,), daemon=True)
+        t.start()
+
+def spawnTCPConnection(connection):
+    while True:
+        tcp_data = connection.recv(4096)
+        print("TCP Received:", tcp_data)
+        for s in subscribers:
+            s.sendall(tcp_data)
+        if not tcp_data:
+            print("Connection Killed")
+            break
+    connection.close()
+    exit()
+
+serverStartup()
