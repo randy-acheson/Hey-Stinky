c2ba296aec11e03bb485f0119f9eea0c5643f4db
Mary Abbott <mabbott9@gatech.edu>
2021-02-20 11:22:56 -0500

    made player prefab

--- Assets/Scripts/ClosetDoorController.cs
+++ Assets/Scripts/ClosetDoorController.cs
@@ -21,7 +21,8 @@ public class ClosetDoorController : MonoBehaviour, InteractiveObject
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Door_open"))
         {
             foreach (GameObject player in playersToHide) {
-
+                player.transform.position = new Vector3(-12.5f, -7.2f, -17.8f);
+                player.transform.rotation.eulerAngles.Set(0f, 67.3f, 0f);
             }
             animator.SetBool("open", false);
             playersToHide.Clear();
