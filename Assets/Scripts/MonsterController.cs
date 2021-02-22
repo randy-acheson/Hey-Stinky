0a790f37dc2caeeabf19c002ee2b888a63c001a0
Mary Abbott <mabbott9@gatech.edu>
2021-02-21 21:08:15 -0500

    other monster script

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -277,7 +277,8 @@ public class MonsterController : MonoBehaviour, CreatureBase
         {
             newAnimatorState = 0;
         }
-        if (animator.GetInteger("movementState") != newAnimatorState)
+        animator.SetInteger("movementState", newAnimatorState);
+        /*if (animator.GetInteger("movementState") != newAnimatorState)
         {
             animator.SetInteger("movementState", newAnimatorState);
             Dictionary<string, string> tcpMoveCommand = new Dictionary<string, string>();
@@ -286,9 +287,9 @@ public class MonsterController : MonoBehaviour, CreatureBase
             tcpMoveCommand["playerHash"] = player_hash;
             tcpMoveCommand["playerHit"] = "";
             AsyncTCPClient.Send(ClientConnection.dictmuncher(tcpMoveCommand));
-        }
+        }*/
 
-        if(isGrounded && (posX !=0 || posZ != 0))
+        if (isGrounded && (posX !=0 || posZ != 0))
         {
             movement = (movement + 1.5f * Mathf.Max(Mathf.Abs(posX), Mathf.Abs(posZ))) % (Mathf.PI*2f);
         }
